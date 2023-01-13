6417be07921a7c2bfbe231efc7dfd08f36588a0c
Mundurujoana <mundujoana@gmail.com>
2023-01-13 09:29:51 +0200

    added django project, app, and templates for views

--- main/views.py
+++ main/views.py
@@ -11,18 +11,18 @@ def home(request):
     if request.method == 'POST':
         question = request.POST.get('question')
         # Call OpenAI API
-        openai.api_key = "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"
+        openai.api_key = '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'
         response = openai.Completion.create(
-            engine="davinci-instruct-beta",
+            engine="davinci",
             prompt=question,
-            max_tokens=100,
+            max_tokens=1000,
             temperature=0.1,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
         )
-        answer = response['choices'][0].text # Get the response from the API
-        return render(request, 'home.html', {'answer': answer})
+        answer = response['choices']# Get the response from the API
+        return render(request, 'home.html', {'answer': answer.text})
     return render(request, 'home.html')
 
 
